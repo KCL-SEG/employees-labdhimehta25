c2071ec47bd5a5c9117bd0f976efbe0b1310fca2
Labdhi Mehta <labdhi.1.mehta@kcl.ac.uk>
2022-10-13 03:17:37 +0100

    Add comission

--- employee.py
+++ employee.py
@@ -1,6 +1,7 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+#class constructor
 class Employee:
     def __init__(self, name, description ="", salary=0, commission=0):
         self.name = name
@@ -18,15 +19,26 @@ class Employee:
         self.salary = hourlyIncome * hoursWorked
         return
 
+    #comission based on fixed bonus received on top of salary
+    def bonusBasedComission(self, fixedBonusValue):
+        self.comission = fixedBonusValue
+        return
+
+    #comission based on number of contracts landed * comission per contract
+    def contractBasedComission(self, comissionPerContract, numOfContractsLanded):
+        self.comission= comissionPerContract * numOfContractsLanded
+        return
+
     #description = information on how the payement was calaculated
     def outputPayInfo(self, description):
         self.description = description
+        return
 
     def get_pay(self):
         totalpayement = self.salary + self.commission
         return totalpayement
 
-    #outputs informaiton on how the payement was calculated 
+    #outputs informaiton on how the payement was calculated
     def __str__(self):
         return self.description
 
@@ -45,18 +57,22 @@ charlie.hourlyBasedSalary(25,100)
 renee = Employee('Renee')
 renee.outputPayInfo("Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800.")
 renee.contractBasedSalary(3000)
+renee.contractBasedComission(200,4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan')
 jan.outputPayInfo("Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410.")
 jan.hourlyBasedSalary(25,150)
+jan.contractBasedComission(220,3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie')
 robbie.outputPayInfo("Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500.")
 robbie.contractBasedSalary(2000)
+robbie.bonusBasedComission(1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel')
 ariel.outputPayInfo("Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200.")
 ariel.hourlyBasedSalary(30,120)
+ariel.bonusBasedComission(600)
